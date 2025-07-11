71adb094baf3387fdda5d923afacc04f9fd8c613
ThunderConstellations <46077000+ThunderConstellations@users.noreply.github.com>
2025-07-11 00:50:04 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("UwU‑HireMeDaddy extension installed");
+  console.log("UwU‑HireMeDaddy installed.");
 });
 
-chrome.runtime.onMessage.addListener((message) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "notify") {
     chrome.notifications.create({
       type: "basic",
       iconUrl: "assets/icon128.png",
-      title: "UwU‑HireMeDaddy",
-      message: message.text
+      title: message.title || "UwU‑HireMeDaddy",
+      message: message.body || "Job application event occurred!"
     });
   }
 });
