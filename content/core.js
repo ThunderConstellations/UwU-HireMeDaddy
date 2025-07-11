c7d29a24e3b951fb25d429977d1af9ac54f26ddc
ThunderConstellations <46077000+ThunderConstellations@users.noreply.github.com>
2025-07-11 00:55:38 -0500

    Update core.js

--- content/core.js
+++ content/core.js
@@ -1,14 +1,17 @@
-import { scheduleApply } from "../scheduler.js";
-import { applyIndeed } from "./site_indeed.js";
-import { applyLinkedIn } from "./site_linkedin.js";
-import { applyGlassdoor } from "./site_glassdoor.js";
-import { applyMonster } from "./site_monster.js";
+(async () => {
+  const host = window.location.hostname;
 
-const host = window.location.hostname;
-let handler = null;
-if (host.includes("indeed.com")) handler = applyIndeed;
-else if (host.includes("linkedin.com")) handler = applyLinkedIn;
-else if (host.includes("glassdoor.com")) handler = applyGlassdoor;
-else if (host.includes("monster.com")) handler = applyMonster;
-
-if (handler) scheduleApply(handler);
+  if (host.includes("linkedin.com")) {
+    const module = await import(chrome.runtime.getURL("content/sites/site_linkedin.js"));
+    module.run();
+  } else if (host.includes("indeed.com")) {
+    const module = await import(chrome.runtime.getURL("content/sites/site_indeed.js"));
+    module.run();
+  } else if (host.includes("glassdoor.com")) {
+    const module = await import(chrome.runtime.getURL("content/sites/site_glassdoor.js"));
+    module.run();
+  } else if (host.includes("monster.com")) {
+    const module = await import(chrome.runtime.getURL("content/sites/site_monster.js"));
+    module.run();
+  }
+})();
